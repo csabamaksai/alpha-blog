3430f43cb461710ec8c7ac37a7fc3a6572db411c
csabamaksai <csmaksai@gmail.com>
2023-11-05 19:00:28 +0100

    Add new article and error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,20 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new # Create a new instance of Article at the first page load so be able to use
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
 end
 
 # Path: app/controllers/articles_controller.rb
