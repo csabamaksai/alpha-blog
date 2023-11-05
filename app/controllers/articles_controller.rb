0d9b8fd3f7a3a2922838e3e5fabcdd89345225e5
csabamaksai <csmaksai@gmail.com>
2023-11-05 08:54:49 +0100

    Added article listing solution

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,10 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def index
+    @articles = Article.all
+  end
 end
 
-# Path:
+# Path: app/controllers/articles_controller.rb
