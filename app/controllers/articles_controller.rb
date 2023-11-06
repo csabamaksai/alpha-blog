b21fe50c73bc97765a38946ac8c3455ad8d671d4
csabamaksai <csmaksai@gmail.com>
2023-11-06 21:28:51 +0100

    All the links and DRYing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -11,14 +11,41 @@ class ArticlesController < ApplicationController
     @article = Article.new # Create a new instance of Article at the first page load so be able to use
   end
 
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
     else
       render 'new', status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was deleted successfully."
+    redirect_to articles_path
+  end
+
+  private # This is a private method that can only be called from within this class
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
 
